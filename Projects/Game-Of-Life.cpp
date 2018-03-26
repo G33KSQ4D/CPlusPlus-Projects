5acd1594810bf86283f3a072f3a14c6d9b26f257
Jonathan Purcell <jonathan_purcell@icloud.com>
2018-03-26 20:46:27 +0000

    Game of life cleanup

--- Projects/Game-Of-Life.cpp
+++ Projects/Game-Of-Life.cpp
@@ -3,12 +3,32 @@
 #define WIDTH 10
 #define HEIGHT 10
 
+void generate_board(char board[HEIGHT][WIDTH]);
+void print_board(char board[HEIGHT][WIDTH]);
+int get_neighbouring_live_cells(int *x, int *y, char board[HEIGHT][WIDTH]);
+
 int main(int argc, char *argv[])
 {
+  // Initializing game grid
   char board[HEIGHT][WIDTH];
 
-  srand(time(0));
+  // Generating random live cells in the grid
+  generate_board(board);
 
+  // Printing the grid cause why not
+  print_board(board);
+
+  int x = 0, y = 0;
+  std::cout << get_neighbouring_live_cells(&x, &y, board) << std::endl;
+
+
+
+  return 0;
+}
+
+void generate_board(char board[HEIGHT][WIDTH])
+{
+  srand(time(0));
   for (int y=0; y<HEIGHT; y++)
   {
     for (int x=0; x<WIDTH; x++)
@@ -19,7 +39,10 @@ int main(int argc, char *argv[])
       (rand()%4==1) ? board[y][x]='O' : board[y][x]=' ';
     }
   }
+}
 
+void print_board(char board[HEIGHT][WIDTH])
+{
   for (int y=0; y<HEIGHT; y++)
   {
     for (int x=0; x<WIDTH; x++)
@@ -29,6 +52,42 @@ int main(int argc, char *argv[])
 
     std::cout << std::endl;
   }
-
-  return 0;
 }
+
+int get_neighbouring_live_cells(int *x, int *y, char board[HEIGHT][WIDTH])
+{
+  int neighbouring_live_cells = 0;
+
+  try {
+    if (board[*y][*x-1] == 'O') neighbouring_live_cells++; // Left
+    if (board[*y][*x+1] == 'O') neighbouring_live_cells++; // Right
+    if (board[*y-1][*x+1] == 'O') neighbouring_live_cells++; // Top
+    if (board[*y+1][*x] == 'O') neighbouring_live_cells++; // Bottom
+
+    if (board[*y-1][*x-1] == 'O') neighbouring_live_cells++; // Top Left
+    if (board[*y-1][*x+1] == 'O') neighbouring_live_cells++; // Top Right
+    if (board[*y+1][*x-1] == 'O') neighbouring_live_cells++; // Bottom Left
+    if (board[*y+1][*x+1] == 'O') neighbouring_live_cells++; // Bottom Right
+  } catch (...) {
+    std::cout << "Damn" << std::endl;
+  }
+
+  return neighbouring_live_cells;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
